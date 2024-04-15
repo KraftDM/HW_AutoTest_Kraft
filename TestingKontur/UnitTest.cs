9d521645432d84a1af3a80237945568744983f4c
Kraft Dmitrii <dmitrymafia4@gmail.com>
2024-04-15 17:34:32 +0300

    Complete Autorization test

--- TestingKontur/UnitTest.cs
+++ TestingKontur/UnitTest.cs
@@ -1,11 +1,14 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 namespace TestingKontur
 {
     public class Tests
     {
+        ChromeDriver driver;
+
         [SetUp]
         public void Setup()
         {
@@ -14,11 +17,36 @@ namespace TestingKontur
         [Test]
         public void Autorization()
         {
-            var driver = new ChromeDriver();
+            driver = new ChromeDriver();
 
             driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/Account/Login");
 
-            Assert.Pass();
+            Thread.Sleep(2000);
+
+
+            var login = driver.FindElement(By.Id("Username"));
+            login.SendKeys("lsv.kraft@mail.ru");
+
+            var password = driver.FindElement(By.Id("Password"));
+            password.SendKeys("!SliM788dD!");
+
+            var enter = driver.FindElement(By.Name("button"));
+            enter.Click();
+
+
+            Thread.Sleep(1000);
+
+            var currentUrl = driver.Url;
+            Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news", "currentUrl = " + currentUrl + "а должен быть https://staff-testing.testkontur.ru/news");
+
+
+            //Assert.Pass();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
         }
     }
 }
